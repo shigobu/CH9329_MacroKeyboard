3ed1371b297fdfa6aa12aec40d6cac9e4e5aa2ff
kasamatsu <kasamatsu@kasa.com>
2023-10-10 14:51:58 +0900

    EEPROM部分作成

--- 202MacroKeyboard.ino
+++ 202MacroKeyboard.ino
@@ -1,4 +1,5 @@
 #include <ResKeypad.h>
+#include <EEPROM.h>
 #include "CH9329_Keyboard.h"
 
 // ボーレート設定値計算マクロ。最後の"+0.5"は、四捨五入させるため？？
@@ -10,7 +11,18 @@
 #define CH9329_PACKET_ADDR  2
 #define CH9329_PACKET_CMD   3
 #define CH9329_PACKET_LEN   4
+#define CH9329_PACKET_DATA  5
 #define CH9329_CMD_READ_MY_HID_DATA  0x87
+
+/* カスタムHIDデータのデータ形式　
+struct myHidData{
+  uint8_t num;          //キー番号
+  uint8_t keys[6];      //キー 先頭が0だったらカスタムボタン(PC側で動作を設定)。それ以外は、キーボードボタン。
+  uint8_t modifiers;    //修飾キー
+}
+*/
+const uint8_t MY_HID_DATA_LEN = 8;      //1 + 1 + 6 = 8
+const uint8_t KEY_CONFIGDATA_LEN = 7;   //カスタムHIDデータからキー番号の領域を無くしたもの。EEPROMに格納されるデータ。
 
 const uint8_t SW_PIN = PIN_PA1;
 const uint8_t LED1_PIN = PIN_PA2;
@@ -62,16 +74,15 @@ ISR(USART0_RXC_vect) {
   if (receivePacketPosition == RECEIVE_PACKET_LENGTH) {
     return;
   }
-
-  
   
   receivePacket[receivePacketPosition] = tempData;
   if (receivePacketPosition == CH9329_PACKET_LEN) {
-    if (tempData > 8) {
+    // 受信データがLENの場合で、値が8より多い場合はエラー停止。
+    if (tempData > MY_HID_DATA_LEN) {
       digitalWriteFast(LED2_PIN, HIGH);
       while(true);
     }
-    receivePacketPosition += 8 - tempData;
+    receivePacketPosition += MY_HID_DATA_LEN - tempData;
   }
   receivePacketPosition++;
 }
@@ -93,11 +104,19 @@ void setup() {
 }
 
 void loop() {
-  if (receivePacketPosition == CH9329_PACKET_LEN) {
+  // 受信バッファインデックスがバッファの最後(+1)を指している場合、受信完了とみなして処理を行う。
+  if (receivePacketPosition == RECEIVE_PACKET_LENGTH) {
     receivePacketPosition = 0;
+    // HIDカスタムデータの場合、解析し、EEPROMに保存。
     if (receivePacket[CH9329_PACKET_CMD] == CH9329_CMD_READ_MY_HID_DATA){
-      
+      uint8_t* hidData = receivePacket + CH9329_PACKET_DATA;
+      uint8_t eepromAddress = hidData[0] * KEY_CONFIGDATA_LEN;
+      for(int i = 1; i < MY_HID_DATA_LEN; i++){
+        EEPROM.update(eepromAddress, hidData[i]);
+        eepromAddress++;
+      }
     }
   }
-  
+
+  int key = keypad.GetKey();
 }
