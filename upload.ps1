f09192e5b5f4aee44331dcf28b17d93f71993971
kasamatsu <kasamatsu@kasa.com>
2023-10-13 10:34:38 +0900

    アップロードスクリプト追加

---
+++ upload.ps1
@@ -0,0 +1,6 @@
+Param(
+    [Alias("p")]
+    $Port = "COM1"
+)
+
+arduino-cli compile -b megaTinyCore:megaavr:atxy2:chip=202,clock=20internal,bodvoltage=4v2,bodmode=disabled,eesave=disable,millis=enabled,startuptime=8,wiremode=mors,printf=default,WDTtimeout=disabled,WDTwindow=disabled -u -p $Port -P serialupdi -t
