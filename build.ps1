76988532a37e31eb96fd71c5a2ab17bf7b0307d0
kasamatsu <kasamatsu@kasa.com>
2023-10-16 10:55:27 +0900

    fqbnをファイルに外だし

--- build.ps1
+++ build.ps1
@@ -1,1 +1,2 @@
-arduino-cli compile -b megaTinyCore:megaavr:atxy2:chip=202,clock=20internal,bodvoltage=4v2,bodmode=disabled,eesave=disable,millis=enabled,startuptime=8,wiremode=mors,printf=default,WDTtimeout=disabled,WDTwindow=disabled
+$fqbn = Get-Content FQBN.txt -Raw
+arduino-cli compile -b $fqbn
--- upload.ps1
+++ upload.ps1
@@ -1,5 +1,5 @@
 Param(
     [string]$Port = "COM1"
 )
-
-arduino-cli compile -b megaTinyCore:megaavr:atxy2:chip=202,clock=20internal,bodvoltage=4v2,bodmode=disabled,eesave=disable,millis=enabled,startuptime=8,wiremode=mors,printf=default,WDTtimeout=disabled,WDTwindow=disabled -u -p $Port -P serialupdi -t
+$fqbn = Get-Content FQBN.txt -Raw
+arduino-cli compile -b $fqbn -u -p $Port -P serialupdi -t
